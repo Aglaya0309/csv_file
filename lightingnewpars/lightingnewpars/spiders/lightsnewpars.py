5f57d01a166567979d0dcc28a19d5ad9c357c445
ElviraKondratova <elvirochka_sh@mail.ru>
2025-05-29 20:19:56 +0300

    done_2

--- lightingnewpars/lightingnewpars/spiders/lightsnewpars.py
+++ lightingnewpars/lightingnewpars/spiders/lightsnewpars.py
@@ -1,16 +1,16 @@
 import scrapy
 import csv
-
+from urllib.parse import urljoin
 
 class LightingnewparsSpider(scrapy.Spider):
     name = "lightingnewpars"
-    allowed_domains = ["divan.ru"]
+    allowed_domains = ["https:/divan.ru"]
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.filename = "divan_lighting_fixed.csv"
-        # Открываем файл с BOM (utf-8-sig) и разделителем ";" для Excel
+
         with open(self.filename, 'w', newline='', encoding='utf-8-sig') as f:
             writer = csv.writer(f, delimiter=';')
             writer.writerow(['Название', 'Цена', 'Ссылка'])
@@ -18,12 +18,16 @@ class LightingnewparsSpider(scrapy.Spider):
     def parse(self, response):
         lights = response.css("div._Ud0k") or response.css("div[data-testid='product-card']")
         for light in lights:
+            relative_url = light.css("a").attrib["href"]
+            absolute_url = urljoin("https://www.divan.ru", relative_url)
+
             item = {
                 "name": light.css("div.lsooF span::text").get(),
                 "price": light.css("div.pY3d2 span::text").get(),
-                "url": light.css("a").attrib["href"]
+                "url": absolute_url
             }
-            # Записываем с разделителем ";" и кодировкой utf-8-sig
+
+
             with open(self.filename, 'a', newline='', encoding='utf-8-sig') as f:
                 writer = csv.writer(f, delimiter=';')
                 writer.writerow([item['name'], item['price'], item['url']])

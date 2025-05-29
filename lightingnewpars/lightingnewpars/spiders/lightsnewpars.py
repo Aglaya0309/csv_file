8942489afd9950913b68c05a4510b27a00efdab8
ElviraKondratova <elvirochka_sh@mail.ru>
2025-05-29 20:31:24 +0300

    3

--- lightingnewpars/lightingnewpars/spiders/lightsnewpars.py
+++ lightingnewpars/lightingnewpars/spiders/lightsnewpars.py
@@ -2,9 +2,10 @@ import scrapy
 import csv
 from urllib.parse import urljoin
 
+
 class LightingnewparsSpider(scrapy.Spider):
     name = "lightingnewpars"
-    allowed_domains = ["https:/divan.ru"]
+    allowed_domains = ["divan.ru"]
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def __init__(self, *args, **kwargs):
@@ -12,23 +13,20 @@ class LightingnewparsSpider(scrapy.Spider):
         self.filename = "divan_lighting_fixed.csv"
 
         with open(self.filename, 'w', newline='', encoding='utf-8-sig') as f:
-            writer = csv.writer(f, delimiter=';')
+            writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_MINIMAL)
             writer.writerow(['Название', 'Цена', 'Ссылка'])
 
     def parse(self, response):
         lights = response.css("div._Ud0k") or response.css("div[data-testid='product-card']")
         for light in lights:
-            relative_url = light.css("a").attrib["href"]
-            absolute_url = urljoin("https://www.divan.ru", relative_url)
-
             item = {
                 "name": light.css("div.lsooF span::text").get(),
                 "price": light.css("div.pY3d2 span::text").get(),
-                "url": absolute_url
+                "url": urljoin("https://www.divan.ru", light.css("a").attrib["href"])
             }
 
-
             with open(self.filename, 'a', newline='', encoding='utf-8-sig') as f:
-                writer = csv.writer(f, delimiter=';')
+                writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_MINIMAL)
                 writer.writerow([item['name'], item['price'], item['url']])
+
             yield item
